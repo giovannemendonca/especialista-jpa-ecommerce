de394716c4d889d4780a9aef2c38b0bbf5d4e0cd
Giovanne Mendonça <giovannemendonca@outlook.com>
2023-12-28 19:45:52 -0300

    criando entidade cliente

---
+++ src/main/java/dev/ecommerce/model/Cliente.java
@@ -0,0 +1,46 @@
+package dev.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity(name = "cliente")
+public class Cliente {
+
+    @Id
+    private Integer id;
+
+    private String nome;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
